5bb969050b8b9b5fb000d6f1792670ac1c5ccba3
ahmed farid <engfaridooo@gmail.com>
2024-09-20 16:22:40 +0300

    Initial Commit

---
+++ GitHubRepositories/GitHubRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesApp.swift
+//  GitHubRepositories
+//
+//  Created by Farido on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
